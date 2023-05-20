d82c5bb9625dd8f257d2b6a87c3ec1b7c3a83d87
tsimkoruslan <tsimkoruslan@gmail.com>
2023-05-20 05:30:38 +0300

    v 0.8.9

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -8,22 +8,25 @@ import bootstrapLogo from '../assets/images/Partner/bootstrap.png'
 import Icons8Logo from '../assets/images/Partner/Icons8_logo.png'
 import gitHubLogo from '../assets/images/Partner/github-mark.png'
 import {HomeLink} from "../Components";
+import {useAppSelector} from "../hooks";
 const WelcomePage = () => {
+    const {toggle} = useAppSelector(state => state.moviesReducer)
+    const dark = toggle ? `${css.Dark} ${css.Main}`: `${css.White} ${css.Main}`
     return (
-        <div className={css.Main} >
+        <div className={dark} >
             <div className={css.Text}>
                 <img src={logo} alt='logo'/>
                 <h4>Welcome to our movie website! We are pleased to greet you here and thank you for choosing us for your
                     cinematic adventure. Here, you will find a vast selection of films across various genres, from
                     blockbusters
                     to classics. Enjoy the magic of cinema and share your impressions!</h4>
-                <h2>To start, please click on the <HomeLink/> button to navigate to the main page.</h2>
+                <h2 className={css.Red}>To start, please click on the <HomeLink/> button to navigate to the main page.</h2>
                 <div>
                     <p>
                         Copyright © 2023 Tsimko Ruslan All Rights Reserved
                         feel free to connect tsimkoruslan@gmail.com
                     </p>
-                    <h6>PARTNER</h6>
+                    <h6 className={css.Red}>PARTNER</h6>
                     <div className={css.Partner}>
                         <img className={css.Img} src={tmdbLogo} alt="tmdb"/>
                         <img className={css.Img} src={octenLogo} alt="octen"/>
